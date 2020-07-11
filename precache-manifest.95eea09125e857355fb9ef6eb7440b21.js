6314e70635cb517d985708840cb25794c095011c
unknown <ldhuy0208@gmail.com>
2020-07-12 03:57:14 +0700

    Updates

---
+++ precache-manifest.95eea09125e857355fb9ef6eb7440b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482e19a5e6b34e8c4a8a6116464efcce",
+    "url": "/test-api-facebook/index.html"
+  },
+  {
+    "revision": "df04039dd143422d9264",
+    "url": "/test-api-facebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e1399931c48c9bca0853",
+    "url": "/test-api-facebook/static/js/2.225d1f3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-api-facebook/static/js/2.225d1f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df04039dd143422d9264",
+    "url": "/test-api-facebook/static/js/main.fb644d71.chunk.js"
+  },
+  {
+    "revision": "84af1767ea1fe11ea29d",
+    "url": "/test-api-facebook/static/js/runtime-main.c5d086fe.js"
+  }
+]);
