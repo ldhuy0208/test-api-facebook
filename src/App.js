8430e5ae73e5637038b99e06ad0b5b800b7f93d7
unknown <ldhuy0208@gmail.com>
2020-07-12 03:55:20 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ message: e.target.value });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const params = {
+      message: this.state.message,
+      access_token:
+        "EAAH8ll3xGecBAJ9umr2Uz4RwjnHjSo7fpleeg8hiILUqIZBoqg7DOUsiznq2DP6HvJ0eDRxBuPSTAkbZByMNocmmbXv1TQoqodCDJB5Q6FaiHesHymperFFpRSCnfUKkCG3mG0lLS0pXhxwsUbM0mq5dQkw1DZBXrJubWYPeT52ZATDMu2b1IXPGEaBEkkwZD",
+    };
+
+    console.log(params);
+
+    try {
+      const res = await Axios.post(
+        "https://graph.facebook.com/1213620598825676/feed?",
+        params
+      );
+      console.log(res);
+      alert(res);
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  };
+  render() {
+    return (
+      <div>
+        <h1>Đăng bài facebook</h1>
+        <form onSubmit={this.onSubmit}>
+          <textarea value={this.state.message} onChange={this.onChange} />
+          <button>Đăng bài</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
